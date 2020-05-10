1eae7a1a86ac0983daa1e194e5d02b4bbbdd9e76
Zaripov Shohruh <shohruhbek.8969@mail.ru>
2020-05-10 10:41:56 +0500

    first commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="wrapper col5">
+  <div id="footer">
+
+    <div id="copyright">
+      <p class="fl_left">Hospital Menegment &copy; 2020 - All Rights Reserved | <a href="adminlogin.php">Admin Login Panel</a> | <a href="doctorlogin.php">Doctor Login Panel</a></p>
+      <p class="fl_right">Developed by <a href="https://www.studentprojectguide.com">www.studentprojectguide.com</a></p>
+      <br class="clear" />
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+</body>
+</html>
